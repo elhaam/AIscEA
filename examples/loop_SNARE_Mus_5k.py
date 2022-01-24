cc413a1b2c318791c7321ba63d1d9b725fef41e4
elhaam <elhamjafarice@gmail.com>
2022-01-24 10:47:26 -0500

    Comments

--- examples/loop_SNARE_Mus_5k.py
+++ examples/loop_SNARE_Mus_5k.py
@@ -19,44 +19,29 @@ for res1 in np.arange(0.1, 1., 0.05):
         input_dir = "/home/ejafari/alignment/downstream/data/SNAREseq/Mouse/5k/"
         # Find marker genes of scRNAseq data
         markers_rna, rna = scRNAseq_clustering_original(input_dir + 'scRNAseq.csv', filtering=True, resl=res1, highly_var=True, tr=True, n_pc=15)
-
-
         cls_r_raw = len(set(rna.obs['leiden']))
         
         # Find clusters of low dimensional cisTopic result for scATACseq
         atac = clustering(input_dir + 'cisTopic_topics.tsv', transpose=True, resl=res2)
-
-
         ############################ scATAC ######################################
         # 1) Put cisTopic clusters and embedding values on the original data and 2) find marker genes and their logFC
         f_original = 'pred_matrix_closest_genes_1k_downstream_strand_first_prom.csv'
         markers_atac, atac_cis_on_org = process_cisTopic_on_orig(atac, f_original, input_dir, save=False, transpose=True)
-
         # To match name of the cells in RNA and ATAC data
         atac_cis_on_org.obs.index = [i.replace('.','-') for i in atac_cis_on_org.obs.index] 
-
-
         cls_a_raw = len(set(atac_cis_on_org.obs['leiden']))
-        
         
         silhouette_avg_r = silhouette_score(rna.obsm['X_umap'], rna.obs['leiden'])
         silhouette_avg_a = silhouette_score(atac_cis_on_org.obsm['X_umap'], atac_cis_on_org.obs['leiden'])
-
         print("silhouette_avg: ", silhouette_avg_r, silhouette_avg_a)
-
-
+        # Extract cluster mapping
         rna, atac_cis_on_org,  markers_rna, markers_atac = rm_tiny_cluster(rna, atac_cis_on_org,markers_rna, markers_atac, min_cells=200, rm_correspondence=False)
-
         col_ind, rna, atac_cis_on_org = extract_mapped_clusters(rna, markers_rna, atac_cis_on_org, markers_atac, p_val_count=6)
-
         try:
             print(rna.shape, atac_cis_on_org.shape)
             overlap = len(set(rna.obs.index) & set(atac_cis_on_org.obs.index))
             print(overlap)
-
             res_df.loc[len(res_df)] = [res1, cls_r_raw, len(set(rna.obs['leiden'])), silhouette_avg_r, res2, cls_a_raw, len(set(atac_cis_on_org.obs['leiden'])), silhouette_avg_a, rna.shape[0], atac_cis_on_org.shape[0], overlap]
         except:
-            res_df.loc[len(res_df)] = [res1, res2,  None, None, None, None, None, None, None, None, 0]
-            
-            
+            res_df.loc[len(res_df)] = [res1, res2,  None, None, None, None, None, None, None, None, 0] 
         res_df.to_csv('5k_loop_params.csv', index=True)
